96ce10d2841d7bf1bbfdcc828c4d2ef0b0a8e5a2
Håvard Pettersson <haavard.pettersson@gmail.com>
2010-11-23 19:02:26 +0100

    Started a different approach to using the omnibox.

--- scripts/omnibox.js
+++ scripts/omnibox.js
@@ -11,73 +11,32 @@
 
 // TODO: i18n
 
-commands =
-{
-    shorten: "shorten -- Shorten a URL using the default shortener (shortcut: sh)",
-    expand: "expand -- Expand a URL using the default expander (shortcut: ex)",
-    using: "using -- Shorten or expand a URL using a given service (shortcut: us)",
-};
+/* usage:
+ *      shorten url
+ *      expand url
+ *      service-id url
+ */
+
+services = get_services();
 
 chrome.experimental.omnibox.onInputChanged.addListener(function(text, suggest)
 {
+    if (text.split(" ")[0] in services) { return; }
+    
     suggests = [];
-    text = text.split(" ");
-    if (text[0] == "using" || text[0] == "us")
+    
+    $.each(services, function(id, service)
     {
-        if (text.length == 1)
+        if (get_service(id).enabled)
         {
-            recent = storage_get("recent_services");
-            $.each(recent, function(id, service_id)
-            {
-                service = get_service(service_id);
-                suggests.push({ content: text[0] + " " + service_id, description: service_id + " -- use " + service.name + " to " + (service.categories.indexOf("expanding") >= 0 ? "expand" : "shorten") + " a URL", descriptionStyles: [chrome.experimental.omnibox.styleMatch(0), chrome.experimental.omnibox.styleDim(service_id.length)] });
-            });
+            suggests.push({ content: id + " " + text, description: (service.categories.indexOf("expanding") >= 0 ? "Expand" : "Shorten") + " using " + service.name });
         }
-        else
-        {
-            $.each(services, function(id, service)
-            {
-                if (id.indexOf(text[1]) >= 0 || service.name.indexOf(text[1]) >= 0)
-                {
-                    suggests.push({ content: text[0] + " " + id, description: id + " -- use " + service.name + " to " + (service.categories.indexOf("expanding") >= 0 ? "expand" : "shorten") + " a URL", descriptionStyles: [chrome.experimental.omnibox.styleMatch(0), chrome.experimental.omnibox.styleDim(id.length)] });
-                }
-            });
-        }
-    }
-    else
-    {
-        $.each(commands, function(command, desc)
-        {
-            if (text[0].substring(0, command.length) == command.substring(0, text[0].length))
-            {
-                styles = [];
-                if (text[0].length > 0)
-                {
-                    styles.push(chrome.experimental.omnibox.styleMatch(0));
-                }
-                styles.push(chrome.experimental.omnibox.styleDim(text[0].length > command.length ? command.length : text[0].length));
-                suggests.push({ content: command + " ", description: desc, descriptionStyles: styles });
-            }
-        });
-    }
+    });
+    
     suggest(suggests);
 });
 
 chrome.experimental.omnibox.onInputEntered.addListener(function(text)
 {
-    text = text.split(" ");
-    if (text[0] == "sh" || text[0] == "shorten")
-    {
-        handle_url(text[1], get_config("shortener"), function(result)
-        {
-            prompt(chrome.i18n.getMessage(result.status ? "successful_shorten_prompt" : "failed_shorten_prompt", service.name) + "\n\n" + chrome.i18n.getMessage("original_url") + "\n" + text[1], result.msg);
-        });
-    }
-    else if (text[0] == "ex" || text[0] == "expand")
-    {
-        handle_url(text[1], get_config("expander"), function(result)
-        {
-            prompt(chrome.i18n.getMessage(result.status ? "successful_expand_prompt" : "failed_expand_prompt", service.name) + "\n\n" + chrome.i18n.getMessage("original_url") + "\n" + text[1], result.msg);
-        });
-    }
+    
 });
