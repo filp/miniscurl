1ed863641fd54e75b119527ee1f10c51d7abb308
Håvard Pettersson <haavard.pettersson@gmail.com>
2011-03-17 23:19:41 +0100

    Continued work on the omnibox interface.

--- scripts/omnibox.js
+++ scripts/omnibox.js
@@ -2,7 +2,7 @@
  * omnibox.js
  * Miniscurl omnibox script
  *
- * Copyright (C) 2010 Håvard Pettersson.
+ * Copyright (C) 2011 Håvard Pettersson.
  *
  * This software is distributed under the GPL Version 2 license.
  * See the attached LICENSE for more information.
@@ -12,8 +12,8 @@
 // TODO: i18n
 
 /* usage:
- *      shorten url
- *      expand url
+ *      [sh]orten url
+ *      [ex]pand url
  *      service-id url
  */
 
@@ -21,15 +21,14 @@ services = get_services();
 
 chrome.experimental.omnibox.onInputChanged.addListener(function(text, suggest)
 {
-    if (text.split(" ")[0] in services) { return; }
-    
+    args = text.split(" ");
     suggests = [];
     
     $.each(services, function(id, service)
     {
-        if (get_service(id).enabled)
+        if (get_service(id).enabled && id.substring(0, args[0].length) == args[0].substring(0, id.length))
         {
-            suggests.push({ content: id + " " + text, description: (service.categories.indexOf("expanding") >= 0 ? "Expand" : "Shorten") + " using " + service.name });
+            suggests.push({ content: id + " " + (args.length > 1 ? args[1] : ""), description: (service.categories.indexOf("expanding") >= 0 ? "Expand" : "Shorten") + " using " + service.name });
         }
     });
     
